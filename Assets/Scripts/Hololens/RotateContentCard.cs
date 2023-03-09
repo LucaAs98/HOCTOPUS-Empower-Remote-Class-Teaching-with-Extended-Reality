69c529e9d6c4fcc19e85db99e0f890d7888b060c
Luca Asunis <lucaasunis@libero.it>
2023-03-09 18:20:41 +0100

    Horizontal menu updated, now it works
    rotation on press and bigger buttons without problems

---
+++ Assets/Scripts/Hololens/RotateContentCard.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RotateContentCard : MonoBehaviour
+{
+    [SerializeField] private GameObject objToRotate;
+    [SerializeField] private float speed = 100;
+
+    void OnEnable()
+    {
+        foreach (var component in GameObject.FindObjectsOfType<RotateContentCard>())
+        {
+            Debug.Log(component + " - " + this.GetComponent<RotateContentCard>());
+            if (component != this.GetComponent<RotateContentCard>())
+            {
+                component.enabled = false;
+                component.objToRotate.gameObject.transform.rotation = new Quaternion(0,0,0,0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objToRotate.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
