593208282492dba07a57d1a8ed010ac21c50cde1
Luca Asunis <lucaasunis@libero.it>
2023-03-31 10:56:09 +0200

    blabla

--- Assets/Scripts/Client/RaiseArm.cs
+++ Assets/Scripts/Client/RaiseArm.cs
@@ -9,12 +9,14 @@ public class RaiseArm : NetworkBehaviour
 {
     public void CallRaiseArm()
     {
+        Debug.Log("Manda notifica da client");
         RaiseArmServerRpc(this.GetComponent<InitClient>().GetPlayerName());
     }
 
     [ServerRpc]
     public void RaiseArmServerRpc(string playerName)
     {
+        Debug.Log("RIchiesta al server di notifica");
         NetworkManager.Singleton.GetComponent<ManageNotification>().AddNotification(playerName);
     }
 }
